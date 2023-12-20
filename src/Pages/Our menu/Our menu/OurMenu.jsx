11aa9fd489725deb8a898c69839c7e1cec533fdd
Nahid-07 <nahidasif1998@gmail.com>
2023-12-20 16:04:13 +0600

    menu page section added

--- src/Pages/Our menu/Our menu/OurMenu.jsx
+++ src/Pages/Our menu/Our menu/OurMenu.jsx
@@ -2,6 +2,9 @@ import { Helmet } from "react-helmet-async";
 import MenuPageFeaturedSection from "../../../components/MenuPageFeaturedSection/MenuPageFeaturedSection";
 import menuPageheaderBG from "../.././../assets/menu/banner3.jpg";
 import dessertsBg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaBg from "../../../assets/menu/pizza-bg.jpg";
+import soupBg from "../../../assets/menu/soup-bg.jpg";
+import saladBg from "../../../assets/menu/salad-bg.jpg"
 import SectionHeader from "../../../components/sectionHeader/sectionHeader";
 import useMenu from "../../hooks/useMenu";
 import Menu from "../../../components/menu/Menu";
@@ -10,6 +13,9 @@ const OurMenu = () => {
   const [menu] = useMenu();
   const todaysOffer = menu.filter((offer) => offer.category === "offered");
   const desserts = menu.filter((dessert) => dessert.category === "dessert");
+  const pizza = menu.filter((pizza) => pizza.category === "pizza");
+  const soup = menu.filter((soup) => soup.category === "soup");
+  const salad = menu.filter((salad) => salad.category === "salad");
   console.log(todaysOffer);
   return (
     <main>
@@ -19,6 +25,8 @@ const OurMenu = () => {
       <section>
         <MenuPageFeaturedSection
           headerMenuBG={menuPageheaderBG}
+          title={"OUR MENU"}
+          subDescription={"Would you like to try a dish?"}
         ></MenuPageFeaturedSection>
       </section>
       <section className="max-w-7xl mx-auto">
@@ -26,7 +34,7 @@ const OurMenu = () => {
           subHeader={"---Don't miss---"}
           mainHeader={"TODAY'S OFFER"}
         ></SectionHeader>
-        <div className="grid mx-5 lg:mx-0 md:grid-cols-2 gap-5 mb-20">
+        <div className="grid mx-5 lg:mx-0 md:grid-cols-2 gap-5 my-20">
           {todaysOffer.map((item) => (
             <Menu key={item._id} item={item}></Menu>
           ))}
@@ -36,15 +44,66 @@ const OurMenu = () => {
       <section>
         <MenuPageFeaturedSection
           headerMenuBG={dessertsBg}
+          title={"DESSERTS"}
+          subDescription={
+            "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          }
         ></MenuPageFeaturedSection>
       </section>
       <section className="max-w-7xl mx-auto">
-        <SectionHeader
-          subHeader={"---Don't miss---"}
-          mainHeader={"TODAY'S OFFER"}
-        ></SectionHeader>
-        <div className="grid mx-5 lg:mx-0 md:grid-cols-2 gap-5 mb-20">
+        <div className="grid mx-5 lg:mx-0 md:grid-cols-2 gap-5 my-20">
           {desserts.map((item) => (
+            <Menu key={item._id} item={item}></Menu>
+          ))}
+        </div>
+      </section>
+      {/* pizza */}
+      <section>
+        <MenuPageFeaturedSection
+          headerMenuBG={pizzaBg}
+          title={"PIZZA"}
+          subDescription={
+            "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          }
+        ></MenuPageFeaturedSection>
+      </section>
+      <section className="max-w-7xl mx-auto">
+        <div className="grid mx-5 lg:mx-0 md:grid-cols-2 gap-5 my-20">
+          {pizza.map((item) => (
+            <Menu key={item._id} item={item}></Menu>
+          ))}
+        </div>
+      </section>
+      {/* soup */}
+      <section>
+        <MenuPageFeaturedSection
+          headerMenuBG={soupBg}
+          title={"SOUP"}
+          subDescription={
+            "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          }
+        ></MenuPageFeaturedSection>
+      </section>
+      <section className="max-w-7xl mx-auto">
+        <div className="grid mx-5 lg:mx-0 md:grid-cols-2 gap-5 my-20">
+          {soup.map((item) => (
+            <Menu key={item._id} item={item}></Menu>
+          ))}
+        </div>
+      </section>
+      {/* salad */}
+      <section>
+        <MenuPageFeaturedSection
+          headerMenuBG={saladBg}
+          title={"SALAD"}
+          subDescription={
+            "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          }
+        ></MenuPageFeaturedSection>
+      </section>
+      <section className="max-w-7xl mx-auto">
+        <div className="grid mx-5 lg:mx-0 md:grid-cols-2 gap-5 my-20">
+          {salad.map((item) => (
             <Menu key={item._id} item={item}></Menu>
           ))}
         </div>
--- src/components/MenuPageFeaturedSection/MenuPageFeaturedSection.jsx
+++ src/components/MenuPageFeaturedSection/MenuPageFeaturedSection.jsx
@@ -1,14 +1,14 @@
 
-const MenuPageFeaturedSection = ({ headerMenuBG }) => {
+const MenuPageFeaturedSection = ({ headerMenuBG,title,subDescription }) => {
   return (
     <div
       style={{ backgroundImage: `url(${headerMenuBG})` }}
-      className="extacss w-full bg-center h-[572px] flex items-center justify-center"
+      className="bg-fixed w-full bg-center h-[572px] flex items-center justify-center"
     >
-      <div className=" bg-black opacity-70 text-white w-6/12 py-20 text-center">
-        <h2 className="text-6xl font-semibold">OUR MENU</h2>
+      <div className=" bg-black opacity-70 text-white mx-5 lg:w-6/12 py-20 text-center px-5">
+        <h2 className="text-6xl font-semibold">{title}</h2>
         <p className="text-xl">
-          Would you like to try a dish?
+          {subDescription}
         </p>
       </div>
     </div>

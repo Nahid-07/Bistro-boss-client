3fc80f8f218d80b8953462cdd98bb8f1309aa7bc
Nahid-07 <nahidasif1998@gmail.com>
2023-12-27 16:56:33 +0600

    get reviews from the server

--- src/Pages/Home/review/Review.jsx
+++ src/Pages/Home/review/Review.jsx
@@ -16,7 +16,7 @@ import { useEffect, useState } from "react";
 const Review = () => {
   const [review, setReview] = useState([]);
   useEffect(() => {
-    fetch("review.json")
+    fetch("http://localhost:5000/review")
       .then((res) => res.json())
       .then((data) => setReview(data));
   }, []);

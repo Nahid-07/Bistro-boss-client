b2b0fd86f8d07f57a2dbda9f0a0dccdb73089a38
Nahid-07 <nahidasif1998@gmail.com>
2024-01-05 15:56:32 +0600

    added login handle

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,24 +1,15 @@
 import { Helmet } from "react-helmet-async";
 import bg from "../../assets/others/authentication.png";
-import {
-  loadCaptchaEnginge,
-  LoadCanvasTemplate,
-  LoadCanvasTemplateNoReload,
-  validateCaptcha,
-} from "react-simple-captcha";
-import { useEffect } from "react";
 
 const Login = () => {
-  useEffect(()=>{
-    loadCaptchaEnginge(6)
-  },[])
-  const handleSubmit = (e) => {
+ 
+  const handleLogin = e =>{
     e.preventDefault();
     const form = e.target;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(email, password);
-  };
+    console.log(email,password);
+  }
   return (
     <div
       style={{ backgroundImage: `url(${bg})` }}
@@ -36,21 +27,17 @@ const Login = () => {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form
-            onSubmit={handleSubmit}
+          onSubmit={handleLogin}
             className="space-y-6"
-            action="#"
-            method="POST"
           >
             <div>
               <label
-                htmlFor="email"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Email address
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
                   name="email"
                   type="email"
                   autoComplete="email"
@@ -63,7 +50,6 @@ const Login = () => {
             <div>
               <div className="flex items-center justify-between">
                 <label
-                  htmlFor="password"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Password
@@ -79,7 +65,6 @@ const Login = () => {
               </div>
               <div className="mt-2">
                 <input
-                  id="password"
                   name="password"
                   type="password"
                   autoComplete="current-password"
@@ -88,32 +73,9 @@ const Login = () => {
                 />
               </div>
             </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Enter the captcha code
-                </label>
-                
-              </div>
-              <div className="mt-2">
-                <LoadCanvasTemplate />
-              </div>
-              <div className="mt-2">
-                <input
-                  name="email"
-                  type="text"
-                  placeholder="enter the captcha"
-                  required
-                  className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+            
             <div>
               <button
-                type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in

58bb27d59da257045da7935bc1c65be41b80b78f
Nahid-07 <nahidasif1998@gmail.com>
2024-01-07 19:04:48 +0600

    added state observer from firebase

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,9 +1,26 @@
-import {createContext, useState } from "react"
+import {createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 const AuthContext = createContext(null);
+const auth = getAuth(app);
+
 const AuthProvider = ({children})=>{
     const [user, setUser] = useState(null);
     const [loader, setLoader] = useState(true);
+
+
+    // user obsever
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser)
+            console.log('current User', currentUser);
+            setLoader(false)
+        })
+        return ()=>{
+            return unsubscribe()
+        }
+    },[])
     const authInfo = {
         user,
         loader

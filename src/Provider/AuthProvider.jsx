f1296cda6b3002ac34ef06be65f2de8b9376f0ca
Nahid-07 <nahidasif1998@gmail.com>
2024-01-07 18:50:07 +0600

    cintext api created for authentication

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,17 @@
+import {createContext, useState } from "react"
+
+const AuthContext = createContext(null);
+const AuthProvider = ({children})=>{
+    const [user, setUser] = useState(null);
+    const [loader, setLoader] = useState(true);
+    const authInfo = {
+        user,
+        loader
+    }
+    return(
+        <AuthContext.Provider value={authInfo}>
+        {children}
+        </AuthContext.Provider>
+    )
+}
+export default AuthProvider

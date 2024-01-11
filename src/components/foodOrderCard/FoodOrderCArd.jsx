409779c8225098ccf86f4f22ffff4d6be4e55287
Nahid-07 <nahidasif1998@gmail.com>
2024-01-11 23:51:37 +0600

    add to cart function button added

--- src/components/foodOrderCard/FoodOrderCArd.jsx
+++ src/components/foodOrderCard/FoodOrderCArd.jsx
@@ -1,5 +1,8 @@
 const FoodOrderCArd = ({ item }) => {
   const { name, recipe, image, price } = item;
+  const handleAddToCart = item =>{
+    console.log(item);
+  }
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
@@ -14,7 +17,7 @@ const FoodOrderCArd = ({ item }) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">Add to cart</button>
+          <button onClick={()=>handleAddToCart(item)} className="btn btn-primary">Add to cart</button>
         </div>
       </div>
     </div>

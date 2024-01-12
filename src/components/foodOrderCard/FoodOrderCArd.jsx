a159caa7a69443b96533b2aae3f977f2d67c0e2e
Nahid-07 <nahidasif1998@gmail.com>
2024-01-12 15:20:49 +0600

    fix some ux

--- src/components/foodOrderCard/FoodOrderCArd.jsx
+++ src/components/foodOrderCard/FoodOrderCArd.jsx
@@ -17,7 +17,7 @@ const FoodOrderCArd = ({ item }) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions">
-          <button onClick={()=>handleAddToCart(item)} className="btn btn-primary">Add to cart</button>
+          <button onClick={()=>handleAddToCart(item)} className="btn bg-indigo-600 text-white">Add to cart</button>
         </div>
       </div>
     </div>

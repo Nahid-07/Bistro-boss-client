248cfcd680df7510de99ddaa19760c88b82e5180
Nahid-07 <nahidasif1998@gmail.com>
2024-01-23 22:51:59 +0600

    added jwt and set it to local storage

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,56 +1,71 @@
-import {createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
-const AuthProvider = ({children})=>{
-    const [user, setUser] = useState(null);
-    const [loader, setLoader] = useState(true);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(true);
 
-    // create user with email and password
-    const emailPasswordUser = (email,password) =>{
-        setLoader(true)
-        return createUserWithEmailAndPassword(auth,email,password)
-    }
-    // update user information
-    const updateUserProfile = (userInfo) =>{
-        return updateProfile(auth.currentUser,userInfo);
-    }
-    // Sign in user 
-    const signinUser = (email,password) =>{
-        setLoader(true)
-        return signInWithEmailAndPassword(auth, email,password)
-    }
-    // signout 
-    const logout = ()=>{
-        return signOut(auth)
-    }
-    // user obsever
-    useEffect(()=>{
-        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
-            setUser(currentUser)
-            setLoader(false)
-        })
-        return ()=>{
-            return unsubscribe()
-        }
-    },[])
-    
-    const authInfo = {
-        user,
-        loader,
-        emailPasswordUser,
-        updateUserProfile,
-        setLoader,
-        signinUser,
-        logout
-    }
-    return(
-        <AuthContext.Provider value={authInfo}>
-        {children}
-        </AuthContext.Provider>
-    )
-}
-export default AuthProvider
+  // create user with email and password
+  const emailPasswordUser = (email, password) => {
+    setLoader(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  // update user information
+  const updateUserProfile = (userInfo) => {
+    return updateProfile(auth.currentUser, userInfo);
+  };
+  // Sign in user
+  const signinUser = (email, password) => {
+    setLoader(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // signout
+  const logout = () => {
+    return signOut(auth);
+  };
+  // user obsever
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then((data) => {
+            localStorage.setItem('Access Token',data.data)
+          });
+      }else{
+        localStorage.removeItem('Access Token')
+      }
+      setLoader(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loader,
+    emailPasswordUser,
+    updateUserProfile,
+    setLoader,
+    signinUser,
+    logout,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+export default AuthProvider;

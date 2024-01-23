2bfee285d5670ac88856532dfbd8caeeab65ec60
Nahid-07 <nahidasif1998@gmail.com>
2024-01-23 20:49:55 +0600

    make admin function has been done

--- src/Pages/Dashbord/AllUser.jsx
+++ src/Pages/Dashbord/AllUser.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { FaUsersCog } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const AllUser = () => {
     const {data: users=[], refetch} = useQuery({
@@ -10,7 +11,58 @@ const AllUser = () => {
             const data = res.json()
             return data
         }
-    })
+    });
+    const handleCreateAdmin = user =>{
+      const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: "btn btn-success",
+          cancelButton: "btn btn-danger"
+        },
+        buttonsStyling: false
+      });
+      swalWithBootstrapButtons.fire({
+        title: `Are you sure you want to make admin ${user.userName}`,
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, Make adimin!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:5000/users/admin/${user._id}`,{
+            method : "PATCH"
+          }).then(res => res.json())
+          .then(data => {
+            console.log(data);
+           
+            refetch()
+          })
+          swalWithBootstrapButtons.fire({
+            title: "Done!",
+            text: `You successfully added ${user.userName} as admin`,
+            icon: "success"
+          });
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire({
+            title: "Cancelled",
+            text: `Make admin ${user.userName} has been cancelled`,
+            icon: "error"
+          });
+        }
+      });
+     
+      // if(data.modifiedCount === 1){
+      //   Swal.fire({
+      //     title: "Done",
+      //     text: `${user.userName} added as admin`,
+      //     icon: "success"
+      //   });
+      // }
+    }
     return (
         <div>
             <div className="overflow-x-auto">
@@ -32,7 +84,9 @@ const AllUser = () => {
                   </td>
                   <td>{user.userEmail}</td>
 
-                  <td><button className="bg-[#D1A054] p-3 rounded-md text-white"><FaUsersCog /></button></td>
+                  <td>{
+                      user.role === 'admin' ? 'Admin' : <button onClick={()=> handleCreateAdmin(user)} className="bg-[#D1A054] p-3 rounded-md text-white"><FaUsersCog /></button>
+                    }</td>
                   <td>
                     <button
                       className='bg-red-600 p-3 rounded-md text-white'

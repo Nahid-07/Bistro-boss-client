863badc647eb019f2b1e67ccf7f1dad399cc9d10
Nahid-07 <nahidasif1998@gmail.com>
2024-01-23 21:57:03 +0600

    added user delete method

--- src/Pages/Dashbord/AllUser.jsx
+++ src/Pages/Dashbord/AllUser.jsx
@@ -4,44 +4,45 @@ import { MdDeleteForever } from "react-icons/md";
 import Swal from "sweetalert2";
 
 const AllUser = () => {
-    const {data: users=[], refetch} = useQuery({
-        queryKey: ['users'],
-        queryFn: async()=>{
-            const res = await fetch('http://localhost:5000/users');
-            const data = res.json()
-            return data
-        }
+  const { data: users = [], refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/users");
+      const data = res.json();
+      return data;
+    },
+  });
+  const handleCreateAdmin = (user) => {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger",
+      },
+      buttonsStyling: false,
     });
-    const handleCreateAdmin = user =>{
-      const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-          confirmButton: "btn btn-success",
-          cancelButton: "btn btn-danger"
-        },
-        buttonsStyling: false
-      });
-      swalWithBootstrapButtons.fire({
+    swalWithBootstrapButtons
+      .fire({
         title: `Are you sure you want to make admin ${user.userName}`,
         text: "You won't be able to revert this!",
         icon: "warning",
         showCancelButton: true,
         confirmButtonText: "Yes, Make adimin!",
         cancelButtonText: "No, cancel!",
-        reverseButtons: true
-      }).then((result) => {
+        reverseButtons: true,
+      })
+      .then((result) => {
         if (result.isConfirmed) {
-          fetch(`http://localhost:5000/users/admin/${user._id}`,{
-            method : "PATCH"
-          }).then(res => res.json())
-          .then(data => {
-            console.log(data);
-           
-            refetch()
+          fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: "PATCH",
           })
+            .then((res) => res.json())
+            .then(() => {
+              refetch();
+            });
           swalWithBootstrapButtons.fire({
             title: "Done!",
             text: `You successfully added ${user.userName} as admin`,
-            icon: "success"
+            icon: "success",
           });
         } else if (
           /* Read more about handling dismissals below */
@@ -50,57 +51,96 @@ const AllUser = () => {
           swalWithBootstrapButtons.fire({
             title: "Cancelled",
             text: `Make admin ${user.userName} has been cancelled`,
-            icon: "error"
+            icon: "error",
           });
         }
       });
-     
-      // if(data.modifiedCount === 1){
-      //   Swal.fire({
-      //     title: "Done",
-      //     text: `${user.userName} added as admin`,
-      //     icon: "success"
-      //   });
-      // }
-    }
-    return (
-        <div>
-            <div className="overflow-x-auto">
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Roll</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody className="text-lg font-semibold">
-              {users.map((user) => (
-                <tr key={user._id}>
-                  <td>
-                    {user.userName}
-                  </td>
-                  <td>{user.userEmail}</td>
+  };
+  // handle delete 
+  const handleDelete = id =>{
+    const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: "btn btn-success",
+    cancelButton: "btn btn-danger"
+  },
+  buttonsStyling: false
+});
+swalWithBootstrapButtons.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonText: "Yes, delete it!",
+  cancelButtonText: "No, cancel!",
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) {
+    fetch(`http://localhost:5000/users/delete/${id}`,{
+      method : "DELETE"
+    }).then(res => res.json()).then(data => {
+      refetch()
+    })
+    swalWithBootstrapButtons.fire({
+      title: "Deleted!",
+      text: "Your file has been deleted.",
+      icon: "success"
+    });
+  } else if (
+    /* Read more about handling dismissals below */
+    result.dismiss === Swal.DismissReason.cancel
+  ) {
+    swalWithBootstrapButtons.fire({
+      title: "Cancelled",
+      text: "Your imaginary file is safe :)",
+      icon: "error"
+    });
+  }
+});
+    
+  }
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Roll</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody className="text-lg font-semibold">
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.userName}</td>
+                <td>{user.userEmail}</td>
 
-                  <td>{
-                      user.role === 'admin' ? 'Admin' : <button onClick={()=> handleCreateAdmin(user)} className="bg-[#D1A054] p-3 rounded-md text-white"><FaUsersCog /></button>
-                    }</td>
-                  <td>
+                <td>
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
                     <button
-                      className='bg-red-600 p-3 rounded-md text-white'
+                      onClick={() => handleCreateAdmin(user)}
+                      className="bg-[#D1A054] p-3 rounded-md text-white"
                     >
-                      <MdDeleteForever />
+                      <FaUsersCog />
                     </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        </div>
-    );
+                  )}
+                </td>
+                <td>
+                  <button onClick={()=>{handleDelete(user._id)}} className="bg-red-600 p-3 rounded-md text-white">
+                    <MdDeleteForever />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUser;

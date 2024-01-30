9eba88a4d24bd3fa05ebe6f470857c4e1f7d46b4
Nahid-07 <nahidasif1998@gmail.com>
2024-01-30 12:02:09 +0600

    contact us page design complte

--- src/Pages/contactUs/contactUs/ContactUs.jsx
+++ src/Pages/contactUs/contactUs/ContactUs.jsx
@@ -55,6 +55,54 @@ const ContactUs = () => {
           </div>
         </div>
       </div>
+      <div className="max-w-7xl mx-auto mb-12">
+        <SectionHeader
+          mainHeader={'CONTACT FORM'}
+          subHeader={'---Send Us a Message---'}
+        ></SectionHeader>
+      <div className="p-10 bg-gray-200 mt-20">
+        <div>
+          <label className="text-lg font-semibold" htmlFor="">
+           Name
+          </label>
+          <br />
+          <input className="input input-bordered w-full" type="text" />
+        </div>
+        <div className="flex w-full mt-5">
+          <div className="w-full mr-5">
+            <label className="text-lg font-semibold" htmlFor="">
+              {" "}
+              Phone
+            </label>
+            <br />
+            <input className="input input-bordered w-full" type="text" />
+          </div>
+          <div className="w-full ml-5">
+            <label className="text-lg font-semibold" htmlFor="">
+              Email
+            </label>
+            <br />
+            <input className="input input-bordered w-full" type="text" />
+          </div>
+        </div>
+        <div className="mt-5">
+          <label className="text-lg font-semibold" htmlFor="">
+            Message
+          </label>{" "}
+          <br />
+          <textarea
+            className="textarea textarea-bordered w-full"
+            name=""
+            id=""
+          ></textarea>
+        </div>
+        <div>
+          <button className="bg-[#D1A054] px-5 py-2 rounded-sm mt-5 text-white">
+            Send message
+          </button>
+        </div>
+      </div>
+      </div>
     </section>
   );
 };

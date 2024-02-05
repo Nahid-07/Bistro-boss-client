72a29af1b235e5f07da4a9d29645614c688ff879
Nahid-07 <nahidasif1998@gmail.com>
2024-02-05 15:59:36 +0600

    add items form registerd with react hook form

--- src/Pages/Dashbord/AddItems.jsx
+++ src/Pages/Dashbord/AddItems.jsx
@@ -1,19 +1,29 @@
+import { useForm } from "react-hook-form";
 import SectionHeader from "../../components/sectionHeader/sectionHeader";
 
 const AddItems = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const onSubmit = (data)=>{
+    console.log(data);
+  }
   return (
     <div className="">
       <SectionHeader
         subHeader={"---What's new?---"}
         mainHeader={"ADD AN ITEM"}
       ></SectionHeader>
-      <div className="lg:w-1/2 mx-auto p-10 bg-slate-200 mt-20">
+      <form onSubmit={handleSubmit(onSubmit)} className="lg:w-1/2 mx-auto p-10 bg-slate-200 mt-20">
         <div>
           <label className="text-lg font-semibold" htmlFor="">
             Racipe Name
           </label>
           <br />
-          <input className="input input-bordered w-full" type="text" />
+          <input {...register("name", { required: true })} className="input input-bordered w-full" type="text" />
         </div>
         <div className="flex w-full mt-5">
           <div className="w-full mr-5">
@@ -22,12 +32,16 @@ const AddItems = () => {
               Category
             </label>
             <br />
-            <select className="select select-bordered w-full max-w-xs">
-              <option disabled selected>
-                Who shot first?
+            <select {...register("category", { required: true })} className="select select-bordered w-full max-w-xs">
+              <option selected>
+               salad
               </option>
-              <option>Han Solo</option>
-              <option>Greedo</option>
+              <option>soup</option>
+              <option>dessert</option>
+              <option>popular</option>
+              <option>pizza</option>
+              <option>offered</option>
+              <option>drinks</option>
             </select>
           </div>
           <div className="w-full ml-5">
@@ -35,7 +49,7 @@ const AddItems = () => {
               Price
             </label>
             <br />
-            <input className="input input-bordered w-full" type="text" />
+            <input {...register("price", { required: true })} className="input input-bordered w-full" type="text" />
           </div>
         </div>
         <div className="mt-5">
@@ -44,9 +58,8 @@ const AddItems = () => {
           </label>{" "}
           <br />
           <textarea
+          {...register("recipe", {required: true})}
             className="textarea textarea-bordered w-full"
-            name=""
-            id=""
           ></textarea>
         </div>
         <div className="mt-5">
@@ -57,7 +70,7 @@ const AddItems = () => {
             Add Items
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
